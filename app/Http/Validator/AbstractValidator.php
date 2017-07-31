79a9e9ec06037a3214d770a1479afbeb8d5f77bc
Wesley Oliveira <wesley.oliveira@zarpsystem.com.br>
2017-07-31 13:59:46 -0300

    Adicionado atributo min

--- app/Http/Validator/AbstractValidator.php
+++ app/Http/Validator/AbstractValidator.php
@@ -5,12 +5,19 @@ namespace App\Http\Validator;
 use Validator;
 use Fig\Http\Message\StatusCodeInterface;
 
+/**
+ * Class AbstractValidator
+ * @package App\Http\Validator
+ */
 abstract class AbstractValidator
 {
+    /* @var array */
     protected $rules = [];
 
+    /* @var array */
     protected $messages = [
         'required'      => 'O campo :attribute e obrigatorio.',
+        'min'           => 'O campo :attribute deve conter no minimo (:min) caracteres.',
         'max'           => 'O campo :attribute e maior que o permitido(:max).',
         'numeric'       => 'O campo :attribute deve ser um numero.'
     ];

f07bc111da0895f7c742b7ef4dda7b5b93726443
Wesley Oliveira <wesley.oliveira@zarpsystem.com.br>
2017-07-31 11:47:53 -0300

    Repositorio da entidade Fornecedor

---
+++ app/Repository/FornecedorRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repository;
+
+/**
+ * Class FornecedorRepository
+ * @package App\Repository
+ */
+class FornecedorRepository extends AbstractRepository implements InterfaceFornecedorRepository
+{
+}

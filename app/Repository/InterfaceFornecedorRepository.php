35d4c548005a437c7ff1842ce442e50573278845
Wesley Oliveira <wesley.oliveira@zarpsystem.com.br>
2017-07-31 11:48:58 -0300

    Interface que poderá conter as operações básicas da entidade Fornecedor

---
+++ app/Repository/InterfaceFornecedorRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repository;
+
+/**
+ * Interface InterfaceFornecedorRepository
+ * @package App\Repository
+ */
+interface InterfaceFornecedorRepository extends DefaultCRUDRepository
+{
+}

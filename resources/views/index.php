9735e80bba3f74755ca0b98e08cdabd539dcac95
Wesley Oliveira <wesley.oliveira@zarpsystem.com.br>
2017-07-31 17:27:23 -0300

    Movido pastas para a public

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,10 +1,32 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
     <head>
+        <meta name="viewport" content="width=device-width, user-scalable=no">
         <meta charset="UTF-8" />
-        <title>Pagarme - Projeto Markwetplace</title>
-        <link rel="stylesheet" href="../css/main.css">
+        <title>Pagarme - Projeto Marketplace</title>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     </head>
     <body>
-
+        <section id="fantasias">
+            <h1>Fantasias</h1>
+            <div class="container">
+                <?php
+                    foreach($fantasias as $fantasia):
+                ?>
+                <div class="fantasy">
+                    <figure class="figure">
+                        <img src="http://lorempixel.com/200/200" alt="<?=$fantasia->getNome()?>" />
+                        <figcaption class="caption">
+                            <?=$fantasia->getNome()?> <span class="price">R$ <?=$fantasia->getPreco()?></span>
+                        </figcaption>
+                    </figure>
+                    <button class="btn-add-cart">ADICIONAR AO CARRINHO</button>
+                </div>
+                <?php
+                    endforeach;
+                ?>
+            </section>
+        </div>
     </body>
+    <script src="<?= $src ?>"></script>
 </html>

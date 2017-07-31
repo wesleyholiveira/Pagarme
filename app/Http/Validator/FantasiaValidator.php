e34b503bee67a7bd9bfd58d58e349542e548ccb3
Wesley Oliveira <wesley.oliveira@zarpsystem.com.br>
2017-07-31 14:01:16 -0300

    Criado arquivo de validação das Fantasias

---
+++ app/Http/Validator/FantasiaValidator.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Validator;
+
+class FantasiaValidator extends AbstractValidator
+{
+    protected $rules = [
+        'id' => 'numeric|required',
+        'descricao' => 'required|min:4|max:60',
+        'valor' => 'required|numeric'
+    ];
+}

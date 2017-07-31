8a342a640c7e5e79278f0ce91a196d73e45dab1a
Wesley Oliveira <wesley.oliveira@zarpsystem.com.br>
2017-07-31 11:52:35 -0300

    Controller responsável para lidar com as requisições referentes as Fantasias

---
+++ app/Http/Controllers/FantasiaController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Factories\FantasiaFactory;
+use App\Repository\FantasiaRepository;
+use App\Repository\FornecedorRepository;
+use Fig\Http\Message\StatusCodeInterface;
+use Illuminate\Http\Request;
+use \Exception;
+
+class FantasiaController extends Controller
+{
+    /* @var FantasiaFactory */
+    protected $fantasiaFactory;
+
+    /* @var FantasiaRepository */
+    protected $fantasia;
+
+    /** @var FornecedorRepository */
+    protected $fornecedor;
+
+    /**
+     * FantasiaController constructor.
+     * @param FantasiaRepository   $fantasia
+     * @param FantasiaFactory      $fantasiaFactory
+     * @param FornecedorRepository $fornecedor
+     */
+    public function __construct(
+        FantasiaRepository $fantasia,
+        FantasiaFactory $fantasiaFactory,
+        FornecedorRepository $fornecedor
+    )
+    {
+        $this->fantasia = $fantasia;
+        $this->fantasiaFactory = $fantasiaFactory;
+        $this->fornecedor = $fornecedor;
+    }
+
+    /**
+     * @param int|null $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function get(int $id = null)
+    {
+        try {
+
+            $response = null;
+
+            if (isset($id) && is_integer($id)) {
+                $response = $this->fantasia->buscar($id);
+            } else {
+                $response = $this->fantasia->findAll();
+            }
+
+            return response()->json(
+                $response,
+                StatusCodeInterface::STATUS_OK
+            );
+        } catch (Exception $e) {
+            return response()->json(
+                ['descricao' => $e->getMessage()],
+                $e->getCode()
+            );
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function post(Request $request)
+    {
+        try {
+            $data = $request->all();
+            $data['id'] = $data['id'] ?? 0;
+            $factory = $this->fantasiaFactory;
+            $fornecedor = $this->fornecedor->buscar($data['fornecedorId']);
+            $fantasia = $factory($data['id'], $data['descricao'], $data['valor'], $fornecedor);
+
+            return response()->json(
+                ['descricao' => $this->fantasia->criar($fantasia)],
+                StatusCodeInterface::STATUS_CREATED
+            );
+        } catch (Exception $e) {
+            return response()->json(
+                ['descricao' => $e->getMessage()],
+                $e->getCode()
+            );
+        }
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function put(Request $request)
+    {
+        try {
+            $data = $request->all();
+            $factory = $this->fantasiaFactory;
+            $fantasia = $factory($data['id'], $data['descricao'], $data['valor']);
+            return response()->json(
+                ['descricao' => $this->fantasia->atualizar($fantasia)],
+                StatusCodeInterface::STATUS_OK
+            );
+        } catch(Exception $e) {
+            return response()->json(
+                ['descricao' => $e->getMessage()],
+                $e->getCode()
+            );
+        }
+    }
+
+    /**
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function delete(int $id)
+    {
+        try {
+            $fantasia = $this->fantasia->buscar($id);
+            return response()->json(
+                ['descricao' => $this->fantasia->deletar($fantasia)],
+                StatusCodeInterface::STATUS_OK
+            );
+        } catch(Exception $e) {
+            return response()->json(
+                ['descricao' => $e->getMessage()],
+                $e->getCode()
+            );
+        }
+    }
+
+}

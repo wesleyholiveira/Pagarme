2c254d3b2fd8e750f65886f155339f283d3fb84b
Wesley Oliveira <wesley.oliveira@zarpsystem.com.br>
2017-07-31 11:48:45 -0300

    Interface que poderá conter as operações básicas da entidade Fantasia

---
+++ app/Repository/InterfaceFantasiaRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repository;
+
+/**
+ * Interface InterfaceFantasiaRepository
+ * @package App\Repository
+ */
+interface InterfaceFantasiaRepository extends DefaultCRUDRepository
+{
+}

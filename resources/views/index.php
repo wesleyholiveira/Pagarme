466563eb93fe96d5e4c7d3137f90255bbde82383
Wesley Wesley <Wesley Wesley>
2017-08-01 00:22:14 -0300

    Melhorias visuais e adicionado efeito no badge

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <meta name="viewport" content="width=device-width, user-scalable=no">
+        <meta name="viewport" content="width=device-width, user-scalable=yes">
         <meta charset="UTF-8" />
         <title>Pagarme - Projeto Marketplace</title>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     </head>
     <body>
@@ -11,22 +12,54 @@
             <h1>Fantasias</h1>
             <div class="container">
                 <?php
-                    foreach($fantasias as $fantasia):
-                ?>
-                <div class="fantasy">
-                    <figure class="figure">
-                        <img src="http://lorempixel.com/200/200" alt="<?=$fantasia->getNome()?>" />
-                        <figcaption class="caption">
-                            <?=$fantasia->getNome()?> <span class="price">R$ <?=$fantasia->getPreco()?></span>
-                        </figcaption>
-                    </figure>
-                    <button class="btn-add-cart">ADICIONAR AO CARRINHO</button>
-                </div>
-                <?php
-                    endforeach;
+                    if(is_array($response)) {
+                        foreach ($response as $fantasia) {
+                            ?>
+                            <div class="fantasy">
+                                <figure class="figure">
+                                    <img src="<?= $fantasia->getImagem()->getUri() ?>"
+                                         alt="<?= $fantasia->getDescricao() ?>"/>
+                                    <figcaption class="caption">
+                                        <?= $fantasia->getDescricao() ?>
+                                        <span class="price">R$ <?= \App\Helpers\Helpers::moneyFormat($fantasia->getValor()) ?></span>
+                                    </figcaption>
+                                </figure>
+                                <button class="btn-add-cart">ADICIONAR AO CARRINHO</button>
+                            </div>
+                            <?php
+                        }
+                    } else {
+                        echo $response;
+                    }
                 ?>
             </section>
+            <ul class="bar">
+                <li>
+                    <a href="#" class="cart">
+                        <i class="badge" id="badget"></i>
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    </a>
+                </li>
+            </ul>
         </div>
     </body>
     <script src="<?= $src ?>"></script>
+    <script>
+       var listaBotoes = document.getElementsByClassName('btn-add-cart');
+       var clicks = 1;
+       for (i = 0; i < listaBotoes.length; i++) {
+           listaBotoes[i].addEventListener('click', function(e) {
+               var badge = document.getElementsByClassName('badge')[0];
+               badge.innerHTML = clicks;
+               badge.className = badge.className.replace(' badge-animation', '');
+
+               setTimeout(function() {
+                   badge.className += ' badge-animation';
+               }, 50);
+
+               clicks++;
+               e.preventDefault();
+           });
+       }
+    </script>
 </html>

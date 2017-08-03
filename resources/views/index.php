926e7bab851764771fa929402fabfceec05dca6b
Wesley Oliveira <wesleyholiveir@gmail.com>
2017-08-03 17:04:40 -0300

    Adicionado campos de envio e requisição ajax para a rota de checkout

--- resources/views/index.php
+++ resources/views/index.php
@@ -24,6 +24,8 @@
                                         <span class="price"><?= \App\Helpers\Helpers::moneyFormat($fantasia->getValor()) ?></span>
                                     </figcaption>
                                 </figure>
+                                <input type="hidden" name="nome" value="<?=$fantasia->getFornecedor()->getNome()?>">
+                                <input type="hidden" name="valor" value="<?=$fantasia->getValor()?>">
                                 <button class="btn-add-cart">ADICIONAR AO CARRINHO</button>
                             </div>
                             <?php
@@ -67,17 +69,34 @@
 
             var itens = [];
 
-//            finalizarCompra();
             cartMenu(itens);
             adicionarAoCarrinho(itens);
+            finalizarCompra(itens);
 
-            function finalizarCompra() {
-                var oReq = new XMLHttpRequest();
-                oReq.onload = function(response) {
-                    console.log(response);
-                };
-                oReq.open("get", "fantasia", true);
-                oReq.send();
+            function finalizarCompra(itens) {
+
+                document.getElementById('finalizarCompra').addEventListener('click', function() {
+                    var http = new XMLHttpRequest();
+                    var btn = this;
+                    var original = btn.innerHTML;
+
+                    btn.disabled = true;
+                    btn.innerHTML = 'Processando';
+
+                    http.onload = function(response) {
+                        btn.disabled = false;
+                        btn.innerHTML = original;
+                    };
+                    http.onreadystatechange = function() {//Call a function when the state changes.
+                        if(http.readyState === 4 && http.status === 200) {
+                            var resposta = JSON.parse(http.responseText);
+                            alert(resposta.descricao);
+                        }
+                    };
+                    http.open("post", "checkout", true);
+                    http.setRequestHeader("Content-Type", "application/json");
+                    http.send(JSON.stringify(itens));
+                });
             }
 
             function cartMenu(itens) {
@@ -100,7 +119,14 @@
                 var j = 0;
                 for (i = 0; i < listaBotoes.length; i++) {
                     listaBotoes[i].addEventListener('click', function(e) {
-                        itens.push(this.parentNode);
+                        var parent = this.parentNode;
+                        var nome = parent.querySelector('input[name="nome"]').value;
+                        var valor = parent.querySelector('input[name="valor"]').value;
+
+                        itens.push({
+                            nome: nome,
+                            valor: valor
+                        });
 
                         finalizarCompra.style.display = 'block';
                         listaSubIcones[j].style.display = 'inline-block';

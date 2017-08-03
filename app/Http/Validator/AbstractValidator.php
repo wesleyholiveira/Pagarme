83d28e05904e16f65d1d48955795872fe4c90966
Wesley Oliveira <wesleyholiveir@gmail.com>
2017-08-03 14:23:07 -0300

    Corrigido erros gramaticais

--- app/Http/Validator/AbstractValidator.php
+++ app/Http/Validator/AbstractValidator.php
@@ -16,10 +16,10 @@ abstract class AbstractValidator
 
     /* @var array */
     protected $messages = [
-        'required'      => 'O campo :attribute e obrigatorio.',
+        'required'      => 'O campo :attribute é obrigatorio.',
         'min'           => 'O campo :attribute deve conter no minimo (:min) caracteres.',
-        'max'           => 'O campo :attribute e maior que o permitido(:max).',
-        'numeric'       => 'O campo :attribute deve ser um numero.'
+        'max'           => 'O campo :attribute é maior que o permitido(:max).',
+        'numeric'       => 'O campo :attribute deve ser um número.'
     ];
 
     /**

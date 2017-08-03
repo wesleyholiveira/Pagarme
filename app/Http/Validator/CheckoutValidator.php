84829e3f4d6a17b2acaa96d42f8a95d5002aa014
Wesley Oliveira <wesleyholiveir@gmail.com>
2017-08-03 14:23:59 -0300

    Criado validator para o checkout

--- app/Http/Validator/CheckoutValidator.php
+++ app/Http/Validator/CheckoutValidator.php
@@ -4,8 +4,9 @@ namespace App\Http\Validator;
 
 class CheckoutValidator extends AbstractValidator
 {
+    /** @var array  */
     protected $rules = [
         'nome' => 'required|min:4|max:60',
-        'valor' => 'required|numeric'
+        'valor' => 'required|numeric',
     ];
 }

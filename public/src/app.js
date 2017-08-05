a4462fb7d4d67376f792e5d8c5a5dc8a9394af7c
Wesley Oliveira <wesleyholiveir@gmail.com>
2017-08-04 21:39:44 -0300

    Correções no arquivo de dependencias

--- public/src/app.js
+++ public/src/app.js
@@ -1,4 +1,4 @@
-require('/application/public/sass/main.scss');
-require('/application/public/node_modules/sweetalert/dist/sweetalert.css');
-require('/application/public/node_modules/sweetalert/dist/sweetalert.min.js');
-require('/application/public/js/main.js');
+require('../sass/main.scss');
+require('../node_modules/sweetalert/dist/sweetalert.css');
+require('../node_modules/sweetalert/dist/sweetalert.min.js');
+require('../js/main.js');
